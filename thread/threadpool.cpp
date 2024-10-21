af5b80b374dfc5f7fb03f620c780f21aaa0371f9
daihan <2877326279@qq.com>
2024-10-21 13:50:58 +0800

    线程池

--- thread/threadpool.cpp
+++ thread/threadpool.cpp
@@ -13,12 +13,52 @@ using namespace std;
 
 class ThreadPool{
 public:
+    ThreadPool(int number){
+        for(int i = 0;i < number;i++){
+            threads.emplace_back([this]{
+                while(1){
+                    unique_lock<mutex> lock(mtx);
+                    condition.wait(lock,[this]{
+                        return !tasks.empty() || stop;
+                    });
+                    if(stop && tasks.empty()){
+                        return;
+                    }
+                    function<void() > task(::move(tasks.front()));
+                    tasks.pop();
+                    lock.unlock();
+                    task();
+                }
+            });
+        }
+    }
+    ~ThreadPool(){
+        {
+            unique_lock<mutex> lock(mtx);
+            stop = true;
+        }
+        condition.notify_all();
+        for(auto &t:threads){
+            t.join();
+        }
+    }
 
-    ~ThreadPool();
+    template<class F,class... Args>
+    void enqueue(F &&f,Args&&... args){
+        function<void()>task = bind(forward<F>(f), forward<Args>(args)...);
+        {
+            unique_lock<mutex> lock(mtx);
+            tasks.emplace(move(task));
+        }
+        condition.notify_one();
+    }
 private:
-    vector<thread> threads;
-    queue <function<void ()>> tasks;
-    mutex mtx;
-    condition_variable condition;
-    bool stop;
+    vector<thread> threads; // 线程数组
+    queue <function<void ()>> tasks;  // 任务队列
+    mutex mtx;  // 互斥信号量
+    condition_variable condition; // 条件变量
+    bool stop;  // 标示位
 };
+int main(){
+    return 0;
+}
